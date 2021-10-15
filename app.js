381019e743bdd7970f7f053be0e8f92428e21b0e
NOVITZ Jean-Philippe <novitz@gmail.com>
2021-10-15 19:05:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -110,12 +110,12 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[i].onclick = () => {
       //if the square below your current square is taken, you can go ontop of it
       if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')) {
-        if (currentPlayer == 1) {
+        if (currentPlayer === 1) {
           squares[i].classList.add('taken')
           squares[i].classList.add('player-one')
           currentPlayer = 2
           displayCurrentPlayer.innerHTML = currentPlayer
-        } else if (currentPlayer == 2){
+        } else if (currentPlayer === 2){
           squares[i].classList.add('taken')
           squares[i].classList.add('player-two')
           currentPlayer = 1
